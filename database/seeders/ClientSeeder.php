e06180a548bfc392c37f0a4373d2d4e27e824b25
AmeerMaavia <65680588+AmeerMaavia@users.noreply.github.com>
2023-07-19 15:37:42 +0500

    basic login signup apis

---
+++ database/seeders/ClientSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Client ;
+use Illuminate\Database\Seeder;
+
+
+class ClientSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Client::firstOrCreate(
+            ['client_id' => 'demas-app-mobile'],
+            ['client_secret' => 'ZGVtYXMtYXBwLW1vYmlsZTtaR1Z0WVhNdFlYQndMVzF2WW1sc1pRPT0=']
+        );
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+// Route::post('register', 'Api\UserController@register');
+
+Route::group(['middleware' => 'auth.client_token'], function () {
+
+    Route::post('register',[UserController::class, 'register']);
+    Route::post('login',[UserController::class, 'login']);
 });

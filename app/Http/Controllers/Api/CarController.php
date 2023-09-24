97ceb5f7daa670b8ca2aa8218deb0a0418f11224
AbubakarMian <41990476+AbubakarMian@users.noreply.github.com>
2023-09-25 00:55:01 +0500

    get all cars

--- app/Http/Controllers/Api/CarController.php
+++ app/Http/Controllers/Api/CarController.php
@@ -10,15 +10,21 @@ class CarController extends Controller
 {
     public function get_all(Request $request){
 
+        $cars = Transport::with('transport_type');
         if($request->transport_type_id){
-            $cars = Transport::
-            with('transport_type')
-            ->where('transport_type_id',$request->transport_type_id)->paginate(500);
+            $cars = $cars->where('transport_type_id',$request->transport_type_id);//->paginate(500);
         }
-        else{
-            $cars = Transport::
-            with('transport_type')->paginate(500);
-        }
+        // else{
+        //     $cars = $cars;
+        // }
+        $cars = $cars->orderByDesc('created_at')->paginate(500);
+
+        $cars->transform(function($item){
+            $item->features = isset($item->features)?explode(',',$item->features):[];
+            $item->booking =  isset($item->booking)?explode(',',$item->booking):[];
+            $item->dontforget = isset($item->dontforget)?explode(',',$item->dontforget):[];
+            return $item;
+        });
         $cars = $cars->items();
         return $this->sendResponse(200,$cars);
     }

5d996be203b9206fa14a1d7c4046091366f990c9
AbubakarMian <41990476+AbubakarMian@users.noreply.github.com>
2023-10-16 19:23:24 +0500

    comments removed

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -49,10 +49,6 @@ class UserController extends Controller
                     $email_handler = new EmailHandler();
                     $email_details = [];
                     $email_details['cc'] =[];
-                    // $email_details['cc'][] = [
-                    //     'from_email' => 'abubakarhere90@gmailcom',
-                    //     'from_name' => 'Abubakar cc',
-                    // ];
                     $email_details['cc'][] = [
                         'from_email' => 'saadyasirthegreat@gmailcom',
                         'from_name' => 'Saad cc',

7bd621cd1632aba8d6d1276cce286967d0664c64
AbubakarMian <41990476+AbubakarMian@users.noreply.github.com>
2023-11-16 07:19:15 +0500

    working on migration

---
+++ database/migrations/2023_11_16_021355_alter_order_order_details_for_payment.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('order', function (Blueprint $table) {
+            $table->dropColumn(['cash_collected_by','payment_type','is_paid']);
+            // $table->string('customer_number')->nullable()->default(null);
+            // $table->float('customer_collection_price',10,2)->default(0);
+        });
+        Schema::table('order_detail', function (Blueprint $table) {
+            // $table->dropColumn('collection_amount_from_user');
+            // $table->float('customer_collection_price',10,2)->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};

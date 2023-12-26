6a75a4a29cae33a77288f705ec8aec4f436b5c3e
AmeerMaavia <65680588+AmeerMaavia@users.noreply.github.com>
2023-12-26 23:03:10 +0500

    wht chk

--- app/Http/Controllers/Report/OrderController.php
+++ app/Http/Controllers/Report/OrderController.php
@@ -175,11 +175,11 @@ class OrderController extends Controller
 
         // $whast_app_urls = $receipt_url;
 
-    //     $whast_app_url = $this->get_absolute_server_url_path($receipt_url);
-    //     // dd($whast_app_urls , $whast_app_url);
+        $whast_app_url = $this->get_absolute_server_url_path($receipt_url);
+        // dd($whast_app_urls , $whast_app_url);
 
-    //    $this->send_url_file_whatsapp('+923343722073',$whast_app_url);
-    //    $this->send_url_file_whatsapp($order->customer_whatsapp_number,$whast_app_url);
+       $this->send_url_file_whatsapp('+923343722073',$whast_app_url);
+       $this->send_url_file_whatsapp($order->customer_whatsapp_number,$whast_app_url);
 
         $email_handler = new EmailHandler();
         $email_details = [];
